e363ea8984b90bc93cc9eb3e8b14eea04e91fd97
Sparky <sp4rkyd4rk@gmail.com>
2021-09-02 16:46:00 +0500

    added .gitignore, first quality of life change

--- README.md
+++ README.md
@@ -3,6 +3,11 @@
 Main principle is simple: we scroll Minecraft's _latest.log_ in real-time, search for a specific keyword in a line and then do the calculation. After the calculation's done we get our result from the terminal AND clipboard, so that we can simply paste the answer. 
 
 ## to-do: 
-- get rid of hard-coded path
+~- get rid of hard-coded path~
 - optimize crashing upon completing
 - make it reusable
+- make script paste the result into the game by itself
+
+## Changelogs: 
+
+_2sep2021_: hard-coded path is now gone, it uses either ENVVAR like "%AppData%" or expands home path. 
--- minequiz.py
+++ minequiz.py
@@ -1,25 +1,34 @@
 import os
 import time
 import subprocess
+import platform
+import pyperclip
 
-# strings = ['[23:16:29]', '[main/INFO]:', '[CHAT]', 'Чат-игра', '»', 'Решите', 'пример:', '676', '+', '898', 'кто', 'первый', 'решит', 'пример,', 'получит:', '30$\n']
+if platform.system() == 'Darwin':
+    logfile = open(os.path.expanduser("~/Library/Application Support/minecraft/logs/latest.log"), "r", encoding="utf-8")
+elif platform.system() == 'Windows':
+    logfile = open(os.getenv("APPDATA") + "/.minecraft/logs/latest.log", "r")
+else:
+    logfile = open(os.path.expanduser("~/.minecraft/logs/latest.log"), "r", encoding="utf-8")
+
 
 def scroll_logs(logfile):
   logfile.seek(0,2)
   while True:
     line = logfile.readline()
     if not line: 
-      time.sleep(0.1)
-      continue
+        time.sleep(0.1)
+        continue
     yield line
 
 
+
 if __name__ == "__main__":
-  logfile = open("/Users/sp4rkyd4rk/Library/Application Support/minecraft/logs/latest.log", "r")
-  lines = scroll_logs(logfile)
-  for line in lines:
-    if "[main/INFO]: [CHAT] Чат-игра" in line:
-    	arr = line.split(" ")
-    	solution = int(arr[7]) + int(arr[9])
-    	print(solution)
-    	subprocess.run("pbcopy", universal_newlines=True, input=str(solution))
+    lines = scroll_logs(logfile)
+    for line in lines:
+        if "[main/INFO]: [CHAT] Чат-игра" in line:
+    	    arr = line.split(" ")
+    	    solution = int(arr[7]) + int(arr[9])
+    	    print(solution)
+    	    pyperclip.copy(solution)
+
